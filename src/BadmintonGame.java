f718efb2a7238387e6d29c5fc07c320975026145
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-03-11 22:52:16 +0530

    Initial commit for badminton game
    started to build a java based badminton game
    I have created a game window using BadmintonGame class

---
+++ src/BadmintonGame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BadmintonGame extends JPanel {
+
+
+    public BadmintonGame() {
+        setBackground(Color.white);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Mini Tennis");
+        BadmintonGame badmintonGame = new BadmintonGame();
+        frame.add(badmintonGame);
+        frame.setSize(700, 600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+
+    }
+
+}
